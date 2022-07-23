22274dd0ec8a83bada2d2b88841fd83c3ca37902
woong <k632ht@gmail.com>
2022-07-24 00:14:15 +0900

    seller service와 통신할때 사용할 feignClient 추가

---
+++ src/main/java/com/chan/customer/dto/SellerOrderRequestDto.java
@@ -0,0 +1,44 @@
+package com.chan.customer.dto;
+
+import com.chan.customer.domain.Address;
+import lombok.*;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Data
+@Builder
+public class SellerOrderRequestDto {
+
+    @NotEmpty
+    private String customerId;
+
+    @NotEmpty
+    private Long customerOrderId;
+
+    @NotEmpty
+    private String customerName;
+
+    @NotEmpty
+    private Address customerAddress;
+
+    @NotEmpty
+    private String customerTelephone;
+
+    @NotEmpty
+    private Long menuId;
+
+    @NotEmpty
+    private String menuName;
+
+    @NotEmpty
+    @Size(min=7, max=7)
+    private String menuPlan;
+
+    @Min(0)
+    private int menuPrice;
+
+    @Min(1)
+    private int menuCount;
+}
