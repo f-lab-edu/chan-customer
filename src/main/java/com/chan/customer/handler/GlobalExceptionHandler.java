ed30e9cb91eebdf1180aca8f0819037359bb1726
revino <lil_w@naver.com>
2022-09-16 00:01:01 +0900

    exception 핸들러 추가

--- src/main/java/com/chan/customer/handler/GlobalExceptionHandler.java
+++ src/main/java/com/chan/customer/handler/GlobalExceptionHandler.java
@@ -3,6 +3,8 @@ package com.chan.customer.handler;
 import com.chan.customer.common.Message;
 import com.chan.customer.common.StatusEnum;
 import com.chan.customer.exception.CustomerFindFailedException;
+import com.chan.customer.exception.OrderFindFailedException;
+import com.chan.customer.exception.OrderRequestFailedException;
 import com.chan.customer.exception.OrderRequestValidationFailedException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,12 +14,22 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomerFindFailedException.class)
-    public ResponseEntity<Message> handleCustomerFindFailException(CustomerFindFailedException ex){
+    public ResponseEntity<Message> handleException(CustomerFindFailedException ex){
+        return responseBadRequest(ex.getMessage());
+    }
+
+    @ExceptionHandler(OrderFindFailedException.class)
+    public ResponseEntity<Message> handleException(OrderFindFailedException ex){
+        return responseBadRequest(ex.getMessage());
+    }
+
+    @ExceptionHandler(OrderRequestFailedException.class)
+    public ResponseEntity<Message> handleException(OrderRequestFailedException ex){
         return responseBadRequest(ex.getMessage());
     }
 
     @ExceptionHandler(OrderRequestValidationFailedException.class)
-    public ResponseEntity<Message> handleOrderRequestValidationFailException(OrderRequestValidationFailedException ex){
+    public ResponseEntity<Message> handleException(OrderRequestValidationFailedException ex){
         return responseBadRequest(ex.getMessage());
     }
 
